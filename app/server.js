7269af356cf2e38d1ec78f78bd4a4e3ca5adb927
Jokubas Dargis <eleventigers@11tserv1.gateway.2wire.net>
2012-07-30 16:45:57 +0100

    init commit

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!\n');
+    res.end('Hello World! Vagrant rocks!\n');
 }).listen(3000);
 
 console.log('Server running on port 3000');
