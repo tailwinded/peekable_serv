0186588b86620533f34992a75f0186f3830b8a45
eleventigers <phonodelik@gmail.com>
2012-08-01 02:13:45 +0100

    filtering crap that Max4Live dicts format to JSON
    Node receives dirty string that gets cleaned and then parsed back to
    JSON

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
   });
 
   socket.on('server_message', function(data){
-   $('#receiver').append('<li>' + data + '</li>');  
+   $('#receiver').append('<li>' + data + '</li>');
+   console.log(data);  
   });
 });
